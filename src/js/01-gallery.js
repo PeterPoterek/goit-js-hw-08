f215e991c5031d183e5a84b8395d62cda980a5d4
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-14 20:13:45 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryItemArr = [];
+
+const handleImageRender = () => {
+  galleryItems.forEach(item => {
+    const galleryItem = document.createElement('div');
+    galleryItem.setAttribute('class', 'gallery__item ');
+
+    const image = document.createElement('img');
+    image.setAttribute('class', 'gallery__image');
+    image.setAttribute('data-source', item.original);
+    image.src = item.preview;
+    image.alt = item.description;
+
+    const link = document.createElement('a');
+    link.setAttribute('href', item.original);
+    link.setAttribute('class', 'gallery__link');
+
+    link.append(image);
+
+    galleryItem.append(link);
+    galleryItemArr.push(galleryItem);
+  });
+
+  gallery.append(...galleryItemArr);
+};
+
+handleImageRender();
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'outside',
+});

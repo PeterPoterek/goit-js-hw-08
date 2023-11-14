67840af99e8aabf060966dbf14ab648ba3e3b352
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-14 23:27:19 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('play', e => {
+  console.log(e);
+
+  const current = e.seconds;
+
+  localStorage.setItem('videoplayer-current-time', current);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+
+  if (currentTime) player.setCurrentTime(currentTime);
+});

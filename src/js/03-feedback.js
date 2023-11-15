13e563f73fe5824addece38993846d23698bc92f
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-15 02:55:00 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,7 +9,11 @@ const handleSubmit = e => {
   const email = e.target.elements.email.value;
   const message = e.target.elements.message.value;
 
-  console.log(email, message);
+  const result = { email: email, message: message };
+  console.log(result);
+
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
 };
 
 feedbackForm.addEventListener('submit', handleSubmit);
@@ -27,6 +31,8 @@ messageTextArea.addEventListener('change', setMessageValue);
 const handlePageReload = () => {
   feedbackForm.elements.email.value = localStorage.getItem('email');
   feedbackForm.elements.message.value = localStorage.getItem('message');
+
+  throttle();
 };
 
 document.addEventListener('DOMContentLoaded', handlePageReload);
